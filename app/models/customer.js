1cb57e92bfc13b69ee4c4a754d9d48092c019b22
Sola-BG <sola.oderinde@gmail.com>
2018-11-13 18:33:50 +0000

    include premises with relationship to customer

--- app/models/customer.js
+++ app/models/customer.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 const { Model } = DS;
-import {attr} from '@ember-decorators/data'
+import {attr, hasMany} from '@ember-decorators/data'
 
 export default class CustomerModel extends Model {
   @attr('string') name;
   @attr('string') email;
+  @hasMany('premise') premises;
 }
